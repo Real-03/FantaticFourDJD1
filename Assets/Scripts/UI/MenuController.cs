6e12d9a563e8f3255f63e2aa11c10daf20d827c3
gcr2000e <a22409619@alunos.ulht.pt>
2025-05-02 15:57:25 +0100

    Menu Scene Upgrade

---
+++ Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,18 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
