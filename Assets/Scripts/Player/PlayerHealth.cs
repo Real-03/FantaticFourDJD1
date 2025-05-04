db18cb2b6e4679e32fb5076cab9671315e7c217e
Tomás Real <tomasreal.art3d@gmail.com>
2025-05-04 19:24:00 +0100

    Change enemys colliders and kick and punch attack area

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -47,7 +47,6 @@ public class PlayerHealth : MonoBehaviour
     void Die()
     {
         Debug.Log($"{gameObject.name} morreu!");
-
         animator.SetTrigger("Die");
         movementScript.enabled = false;
         gameObject.SetActive(false);
