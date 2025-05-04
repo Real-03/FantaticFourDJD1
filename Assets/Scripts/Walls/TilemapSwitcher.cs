2151219d3f542513734fa3d78f2d426d41ecbde8
Tomás Real <tomasreal.art3d@gmail.com>
2025-05-04 20:54:48 +0100

    Bug fixs found Playing, Bridge dont break, Tree dont fall

--- Assets/Scripts/Walls/TilemapSwitcher.cs
+++ Assets/Scripts/Walls/TilemapSwitcher.cs
@@ -16,15 +16,17 @@ public class TilemapSwitcher : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Beast"))
+        if (collision.gameObject.CompareTag("Thing"))
         {
-            Debug.Log("Teste");
+            
             if (tilemapToHide != null)
             {
+                Debug.Log("Desactive");
                 tilemapToHide.gameObject.SetActive(false); // Esconde o primeiro tilemap
             }
             if (tilemapToShow != null)
             {
+                Debug.Log("Active");
                 tilemapToShow.gameObject.SetActive(true); // Mostra o segundo tilemap
             }
         }
