b13ec5c239f26768db02c004eca14996e86ee026
Tomás Real <tomasreal.art3d@gmail.com>
2025-05-10 11:57:20 +0100

    Revert "Upgraded menus"
    verts commit d57e1163c2f9efb881409f72dd2d4dbb858892b1.

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -1,57 +1,18 @@
+
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-    public Button PlayButton;
-    public Button TutorialButton;
-    public Button OptionsButton;
-    public Button QuitButton;
-
-    void Start()
+    public void PlayGame()
     {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-
-        EventSystem.current.SetSelectedGameObject(PlayButton.gameObject);
-
-        // Liga os botões às funções
-        PlayButton.onClick.AddListener(Play);
-        OptionsButton.onClick.AddListener(Options);
-        TutorialButton.onClick.AddListener(Tutorial);
-        QuitButton.onClick.AddListener(Quit);
-    }
-
-    void Update()
-    {
-        if (EventSystem.current.currentSelectedGameObject == null)
-        {
-            EventSystem.current.SetSelectedGameObject(PlayButton.gameObject);
-        }
-    }
-
-    void Play()
-    {
-        Debug.Log("Play");
         SceneManager.LoadScene(1);
     }
 
-    void Tutorial()
+    public void QuitGame()
     {
-        Debug.Log("Tutorial");
-    }
-
-    void Options()
-    {
-        Debug.Log("Options");
-        
-    }
-
-    void Quit()
-    {
-        Debug.Log("Quit");
         Application.Quit();
+        Debug.Log("Quit");
     }
 }
