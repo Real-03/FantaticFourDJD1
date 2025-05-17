a596d477575582258ff2a1d8afdc9a53dd543f61
Tomás Real <tomasreal.art3d@gmail.com>
2025-05-17 22:29:18 +0100

    wevert "Updated beacon sys"
    This reverts commit ce73df0a8985c770fbba11e4f5982c3af54bfc98.

--- Assets/Scripts/Beacon/BeaconScript.cs
+++ Assets/Scripts/Beacon/BeaconScript.cs
@@ -1,55 +1,33 @@
 using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Animations;
 
 public class BeaconScript : MonoBehaviour
 {
-    [Header("Animation Settings")]
-    [SerializeField] private GameObject BeaconOff;
-    [SerializeField] private GameObject BeaconOn;
-    [SerializeField] private GameObject BeamOn;
-    [SerializeField] private GameObject Beam; //Objeto do raio
+    private Animation BeamAnim;
+    private Animation BeaconAnim;
+    public GameObject[] Beacons;
 
-    [Header("Beacon Detection System Settings")]
-    [SerializeField] private Transform BeaconCheck;
-    [SerializeField] private float BeaconCheckRadius = 2.0f;
-    [SerializeField] private LayerMask PlayerLayer; //Layer do jogador
-    private SpriteRenderer spriteRenderer;
-    private bool isActivated = false;
-    private bool isActivating = false;
-
-    void Start()
+    public void OnStart()
     {
-        
+        BeamAnim = GetComponent<Animation>();
+        BeaconAnim = GetComponent<Animation>();
     }
 
-    void Update()
-    {
-        ComputeBeaconState();
-    }
-
-    private void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Beam.SetActive(true);
-            Beam.SetActive(false);
-            BeaconOff.SetActive(false);
-            BeaconOn.SetActive(true);
+            StartCoroutine(BeamActive());
+            BeaconAnim.Play();
         }
     }
 
-    void ComputeBeaconState()
+    IEnumerator BeamActive()
     {
-        Collider2D collider = Physics2D.OverlapCircle(BeaconCheck.position, BeaconCheckRadius, PlayerLayer);
-        isActivated = collider != null;
-    }
-
-    void OnDrawGizmos()
-    {
-        if (BeaconCheck == null) return;
-        Gizmos.color = new Color(1, 0, 0, 0.5f);
-        Gizmos.DrawWireSphere(BeaconCheck.position, BeaconCheckRadius);
+        yield return new WaitForSeconds(0.2f);
+        BeamAnim.Play();
+        yield return new WaitForSeconds(0.2f);
+        BeamAnim.Stop();
     }
 }
